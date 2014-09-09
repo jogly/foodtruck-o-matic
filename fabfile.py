4c1f7c9b84400c4cd9a5f2b4121224f34cb2b111
Joseph Gilley <joe.gilley@gmail.com>
2014-09-09 14:08:19 -0400

    Bug fixes and code cleanup

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,6 @@
-from fabric.api import run, sudo, cd, abort, settings
+from fabric.api import run, sudo, cd, abort, settings, env
 
-def init(host):
-  if host:
-    env.hosts.append(host)
+def init():
 
   project_path = '/home/ubuntu/foodtruck'
 
@@ -11,32 +9,38 @@ def init(host):
       abort('git install failed')
 
   if not has_directory(project_path):
-    if run('git clone git@git://github.com/joegilley/foodtruck-o-matic.git foodtruck').failed:
+    if run('git clone https://github.com/joegilley/foodtruck-o-matic.git foodtruck').failed:
       abort('git cloning failed')
 
   with cd(project_path):
+    safe_install_package('python-pip')
+    safe_install_package('python-virtualenv')
+    safe_install_package('libpq-dev')
+    safe_install_package('postgresql')
+    safe_install_package('postgis')
+    safe_install_package('python-dev')
+
     with settings(warn_only=True):
-      if not has_package('python-pip'):
-        if sudo('apt-get install python-pip').failed:
-          abort('pip install failed')
-
-      if not has_package('python-virtualenv'):
-        if sudo('apt-get install python-virtualenv').failed:
-          abort ('virtualenv install failed')
-
       if run('virtualenv .').failed:
         abort('could not create virtualenv')
 
-      if run('source bin/activate').failed:
-        abort('could not activate virutalenv')
-
-      if run('pip install -r requirements.txt').failed:
-        abort('pip could not install requirements')
+      if run('source bin/activate && pip install -r requirements.txt').failed:
+        abort('could not activate and install virutalenv')
 
 def has_package(package):
   with settings(warn_only=True):
-    return run('dpkg -l | grep -qw {}'.format(package)).failed
+    return not run('dpkg -l | grep -qw {}'.format(package)).failed
 
 def has_directory(dir):
   with settings(warn_only=True):
-    return run('test -d {}'. format(dir)).failed
+    return not run('test -d {}'. format(dir)).failed
+
+def install_package(package):
+  with settings(warn_only=True):
+    return not sudo('apt-get -qq install {}'.format(package)).failed
+
+def safe_install_package(package):
+  with settings(warn_only=True):
+    if not has_package(package):
+      if not install_package(package):
+        abort('could not install {}'.format(package))
