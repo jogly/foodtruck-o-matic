0ba14a8511938a59cf1ca59bb750095bacbfd965
Joseph Gilley <joe.gilley@gmail.com>
2014-09-09 15:56:08 -0400

    Added an nginx conf file

--- fabfile.py
+++ fabfile.py
@@ -1,22 +1,41 @@
 from fabric.api import run, sudo, cd, abort, settings, env
 
+env.hosts = ['ubuntu@ws01.josephgilley.com']
+env.key_filename = '/Users/joma/.ssh/aws-ec2-01'
+
+project_path = '/home/ubuntu/foodtruck'
+load_data_script = '\\copy foodtruck(location_id, applicant, facility_type, cnn, location_description, address, blocklot, block, lot, permit, status, food_items, x, y, latitude, longitude, schedule_url, noi_sent_on, approved_at, received_at, prior_permit, expires_on) from \'{project_path}/foodtrucks_raw.csv\' DELIMITERS \',\' CSV HEADER;'.format(project_path=project_path)
+
+def shove():
+  if has_package('git'):
+    if not has_directory(project_path):
+      if run('git clone https://github.com/joegilley/foodtruck-o-matic.git foodtruck').failed:
+        abort('git cloning failed')
+    with cd(project_path):
+      if run('git pull').failed:
+        abort('shoving git failed')
+
 def init():
 
-  project_path = '/home/ubuntu/foodtruck'
-
-  if not has_package('git'):
-    if sudo('apt-get install git').failed:
-      abort('git install failed')
+  safe_install_package('git')
 
   if not has_directory(project_path):
     if run('git clone https://github.com/joegilley/foodtruck-o-matic.git foodtruck').failed:
       abort('git cloning failed')
 
   with cd(project_path):
+    if run('git pull').failed:
+      abort('git pulling failed')
+
+    safe_install_package('build-essential')
+    safe_install_package('uwsgi')
     safe_install_package('python-pip')
     safe_install_package('python-virtualenv')
     safe_install_package('libpq-dev')
+    safe_install_package('libyaml')
     safe_install_package('postgresql')
+    safe_install_package('postgresql-contrib')
+    safe_install_package('postgresql-9.3-postgis-2.1')
     safe_install_package('postgis')
     safe_install_package('python-dev')
 
@@ -24,8 +43,22 @@ def init():
       if run('virtualenv .').failed:
         abort('could not create virtualenv')
 
-      if run('source bin/activate && pip install -r requirements.txt').failed:
+      if run('source bin/activate && pip install -qr requirements.txt').failed:
         abort('could not activate and install virutalenv')
+
+      if run('psql postgres -tAc "select 1 from pg_roles where rolname=\'{}\'" | grep -q 1'.format('ubuntu')).failed:
+        sudo('createuser ubuntu', user='postgres')
+
+      if not run('psql -l | grep -qw foodtruck').failed:
+        sudo('dropdb foodtruck', user='postgres')
+      sudo('createdb foodtruck', user='postgres')
+
+      sudo('psql -c "CREATE EXTENSION postgis;" foodtruck', user='postgres')
+
+      run('source bin/activate && python manage.py db upgrade')
+
+      if run('psql -c "{};" foodtruck'.format(load_data_script)).failed:
+        abort('failed to load data')
 
 def has_package(package):
   with settings(warn_only=True):
