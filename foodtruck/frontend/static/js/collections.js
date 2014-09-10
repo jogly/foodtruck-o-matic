655bec3f3188830b4ba82250c7cd24db2d312b39
Joseph Gilley <joe.gilley@gmail.com>
2014-09-10 11:27:07 -0400

    upped the count

--- foodtruck/frontend/static/js/collections.js
+++ foodtruck/frontend/static/js/collections.js
@@ -2,12 +2,15 @@ ft.Foodtrucks = Backbone.Collection.extend({
   model: ft.Foodtruck,
   url: function() {
     if (this.coords) {
-      return '/api/foodtrucks/nearby?latitude='+this.coords.latitude+'&longitude='+this.coords.longitude+'&per_page=2';
+      return '/api/foodtrucks/nearby?latitude='+this.coords.latitude+'&longitude='+this.coords.longitude+'&per_page=10';
     } else {
       return 'api/foodtrucks';
     }
   },
   parse: function(response) {
     return response.foodtrucks;
+  },
+  select: function(model) {
+
   }
 });
