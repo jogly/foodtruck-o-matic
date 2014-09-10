16dbeb74d9ab903fdc5c50e49f9fc5bd6d7161c4
Joseph Gilley <joe.gilley@gmail.com>
2014-09-10 02:57:59 -0400

    bug fix

--- manage.py
+++ manage.py
@@ -5,8 +5,8 @@ from foodtruck import create_app, db
 
 application = create_app(config_from_env='APP_SETTINGS')
 
-migrate = Migrate(app, db)
-manager = Manager(app)
+migrate = Migrate(application, db)
+manager = Manager(application)
 
 manager.add_command('db', MigrateCommand)
 
