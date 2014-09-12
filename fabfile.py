3eff1713775f40b34e3311973b8634f53a6681b1
Joseph Gilley <joe.gilley@gmail.com>
2014-09-11 23:25:23 -0400

    changed order

--- fabfile.py
+++ fabfile.py
@@ -56,9 +56,9 @@ def init():
 
       sudo('psql -c "CREATE EXTENSION postgis;" foodtruck', user='postgres')
 
-      run('psql -d foodtruck -c "update foodtruck set location = ST_GeomFromText(\'POINT(\' || latitude || \' \' || longitude || \')\', 4326)"')
-
       run('source bin/activate && python manage.py db upgrade')
+
+      run('psql -d foodtruck -c "update foodtruck set location = ST_GeomFromText(\'POINT(\' || latitude || \' \' || longitude || \')\', 4326)"')
 
       if run('psql -c "{};" foodtruck'.format(load_data_script)).failed:
         abort('failed to load data')
